f2cc476aaed4467c9a7d9ec1021744b40a8c8471
duhsokola <dima_liebi@hotmail.com>
2014-03-25 12:58:34 +0100

    -

--- source/installservice.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /LogToConsole=true "C:\Users\vmadmin\Source\Repos\keylogger\source\KeyLogger\bin\Debug\KeyLogger.exe"
-PAUSE
--- source/uninstallservice.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "C:\Users\vmadmin\Source\Repos\keylogger\source\KeyLogger\bin\Debug\KeyLogger.exe"
-PAUSE
